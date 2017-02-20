e80cf00bd1279f4b70b7c7ea1b9fd6638c2c5c9a
alexkernphysiker <kernphysiker@openmailbox.org>
2017-02-20 23:48:08 +0100

    RootSorter wrap moved to submodule

--- Analysis/analysis-setup.h
+++
@@ -1,8 +0,0 @@
-// this file is distributed under 
-// GPL license
-#ifndef DIQFUKBJVSPNWOVE
-#define DIQFUKBJVSPNWOVE
-#include <functional>
-#include "analysis.h"
-void SetAnalysisType(std::function<Analysis*()>create_analysis);
-#endif
