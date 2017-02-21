1874e954b553a4362f48e9cc4c3c07223e6a9c49
rundel <alexrndl@gmail.com>
2017-02-21 16:15:13 +0100

    script upgrade

--- scripts/RunAnalysis-data.sh
+++ scripts/RunAnalysis-data.sh
@@ -11,11 +11,7 @@ for X in `seq 45873 1 46884`; do
 			else
 				echo "...starting..."
 				echo "#!/bin/bash" >> ${scriptname}
-				echo "rm -f ${RUNS_TMP}/run_${X}" >> ${scriptname}
-				echo "xzcat ${RUNS_DATA}/run_${X}.xz > ${RUNS_TMP}/run_${X}" >> ${scriptname}
-				echo "./rawanalysis Data -local -fin cluster:${RUNS_TMP}/run_${X} -r ${X} -n ${WASA_OUTPUT_DATA}/Data_run_${X} -lf run_${X}.log -abort" >> ${scriptname}
-				echo "rm -f ${RUNS_TMP}/run_${X}" >> ${scriptname}
-				echo >> ${scriptname}
+				echo "xzcat ${RUNS_DATA}/run_${X}.xz|./rawanalysis Data -local -fin cluster: -r ${X} -n ${WASA_OUTPUT_DATA}/Data_run_${X} -lf run_${X}.log -abort" >> ${scriptname}
 				echo "rm -f $PWD/${scriptname}" >> ${scriptname}
 				chmod u+x ${scriptname}
 				./${scriptname}
