73f1296c4311428c812a698004f5c95d866a150f
rndl-b-2-26 <kernphysiker@openmailbox.org>
2017-05-10 19:26:19 +0200

    file added

---
+++ Simulation/runpluto.h
@@ -0,0 +1,7 @@
+// this file is distributed under
+// GPL license
+#ifndef ______RUNPLUTO_H_____
+#define ______RUNPLUTO_H_____
+#include <string>
+void SimulatePluto(const std::string&reaction,const std::string&filename,const double&from,const double&to);
+#endif
