edb58574e3669741e7b76c6f8a86fc1fa10243e1
rndl-b-2-26 <kernphysiker@openmailbox.org>
2017-05-24 23:52:58 +0200

    script upgrade. The following run formats are accepted: unpacked, bz2, xz

--- scripts/RunAnalysis-data.sh
+++ scripts/RunAnalysis-data.sh
@@ -1,24 +1,35 @@
 for X in `seq 45934 1 46884`; do
 	echo "run #${X}..."
-	if [ -e ${RUNS_DATA}/run_${X}.xz ]; then
-		echo "...present"
-		if [ -e ${WASA_OUTPUT_DATA}/Data_run_${X}.root ]; then
-			echo "...has been analyzed"
+	if [ -e ${WASA_OUTPUT_DATA}/Data_run_${X}.root ]; then
+		echo "...has been analyzed"
+	else
+		RUNCAT=""
+		if [ -e ${RUNS_DATA}/run_${X} ]; then
+			RUNCAT="cat ${RUNS_DATA}/run_${X}"
+		fi
+		if [ -e ${RUNS_DATA}/run_${X}.xz ]; then
+			RUNCAT="xzcat ${RUNS_DATA}/run_${X}.xz"
+		fi
+		if [ -e ${RUNS_DATA}/run_${X}.bz2 ]; then
+			RUNCAT="bzcat ${RUNS_DATA}/run_${X}.bz2"
+		fi
+		echo "${RUNCAT}"
+		if [ "${RUNCAT}" == "" ]; then
+			echo "none."
 		else
+			echo "exists."
 			scriptname="run_${X}$1.sh"
 			if [ -e ${scriptname} ]; then
 				echo "...already in process"
 			else
 				echo "...starting..."
 				echo "#!/bin/bash" >> ${scriptname}
-				echo "xzcat ${RUNS_DATA}/run_${X}.xz|./rawanalysis Data -local -fin cluster: -r ${X} -n ${WASA_OUTPUT_DATA}/Data_run_${X}$1 -lf run_${X}$1.log -abort" >> ${scriptname}
+				echo "${RUNCAT}|./rawanalysis Data -local -fin cluster: -r ${X} -n ${WASA_OUTPUT_DATA}/Data_run_${X}$1 -lf run_${X}$1.log -abort" >> ${scriptname}
 				echo "rm -f $PWD/${scriptname}" >> ${scriptname}
 				chmod u+x ${scriptname}
 				./${scriptname}
 			fi
 		fi
-	else
-		echo "...not present"
 	fi
 done
 echo "FINISHED"
