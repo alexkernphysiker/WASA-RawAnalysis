2a32cc2b7d01ae4e7e0b0f77db8cb5fb7f729d29
rndl-b-2-26 <kernphysiker@openmailbox.org>
2018-01-18 12:06:15 +0100

    pluto simulation: unnecessary code removed

--- Simulation/sim-elastic.cpp
+++
@@ -1,8 +0,0 @@
-// this file is distributed under
-// GPL license
-#include <Experiment/experiment_conv.h>
-#include "runpluto.h"
-int main(){
-    SimulatePluto("p d","pd",p_beam_low,p_beam_hi);
-    return 0;
-}
--- Simulation/sim-forward.cpp
+++ Simulation/sim-forward.cpp
@@ -3,7 +3,6 @@
 #include <Experiment/experiment_conv.h>
 #include "runpluto.h"
 int main(){
-    SimulatePluto("He3 eta","He3eta",1.573,p_beam_hi);
     SimulatePluto("He3 pi0","He3pi0",p_beam_low,p_beam_hi);
     SimulatePluto("He3 pi0 pi0","He3pi0pi0",p_beam_low,p_beam_hi);
     SimulatePluto("He3 pi0 pi0 pi0","He3pi0pi0pi0",p_beam_low,p_beam_hi);
