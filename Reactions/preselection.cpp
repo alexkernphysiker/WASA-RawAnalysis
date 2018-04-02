5b3e94a84dcabc3dbd3ad99f4af0bc4e9383a1c7
rundel <alexrndl@gmail.com>
2018-04-02 21:42:43 +0300

    preselection: more strict conditions

--- Reactions/preselection.cpp
+++ Reactions/preselection.cpp
@@ -14,6 +14,37 @@ void Preselection(Analysis&res){
 	TString Tstream1 = Form("file:/home/PRESEL/run_%i.bz2", RunNumber);
 	gWasa->AddOutput("run_presel",Tstream1);
 
-	res.Trigger(trigger_he3_forward.number).pre()<< [](){gWasa->SaveEvent("run_presel");return true;};
-	res.Trigger(trigger_elastic1.number).pre()<< [](){gWasa->SaveEvent("run_presel");return true;};
+        //Preselection for 3He X reactions
+        static long he3count;
+        res.Trigger(trigger_he3_forward.number).pre()<<[](){he3count=0;return true;};
+        static particle_kine he3;
+        res.Trigger(trigger_he3_forward.number).per_track()<<(make_shared<ChainCheck>()
+	    <<ForwardHe3Reconstruction("He3Forward",res,he3)
+	    <<[](){he3count++;return true;}
+        );
+        res.Trigger(trigger_he3_forward.number).post()<<[](){
+            if(he3count>0)gWasa->SaveEvent("run_presel");
+            return true;
+        };
+
+        //Preselection for quasielastic scattering
+        static SortedPoints<double,track_info> tracks;
+	res.Trigger(trigger_elastic1.number).pre()<<[](){tracks.clear();return true;};
+	res.Trigger(trigger_elastic1.number).per_track()<<(make_shared<ChainCheck>()
+                <<[](WTrack&T){return T.Type()==kCDC;}
+                <<[](WTrack&T){return T.Theta()*180./PI()>23.;}
+                <<[](WTrack&T){return T.Edep()>0.03;}
+                <<[](WTrack&T){
+                        double phi=T.Phi();
+                        tracks<<point<double,track_info>(
+                                T.Theta(),
+                                {.L=lorentz_byEkM(double(T.Edep()),Particle::p().mass(),direction(phi,T.Theta())),.t=T.Time()}
+                        );
+                        return true;
+                }
+	);
+        res.Trigger(trigger_elastic1.number).post()<<[](){
+                if(tracks.size()>=2)gWasa->SaveEvent("run_presel");
+                return true;
+        };
 }
