16e21dcd6b8be37478460b7cc703f84a7b5207ca
rndl-b-2-26 <kernphysiker@openmailbox.org>
2018-04-23 15:48:54 +0200

    3He gamma: cuts positions changed

--- Reactions/central.cpp
+++ Reactions/central.cpp
@@ -178,7 +178,7 @@ void Search3He6Gamma(Analysis&res){
 			<< make_shared<Hist1D>("He3nCentralGammas6","GIM3",ggggggim)
 			<< make_shared<SetOfHists1D>("He3nCentralGammas6","TIM3",Q_axis_full(res),he3ggggggimdiff)
                         <<[ggggggmm](WTrack&T)->bool{                                                                 
-                                return (ggggggmm(T)>2.6)&&(ggggggmm(T)<3.0);                                          
+                                return (ggggggmm(T)>2.5)&&(ggggggmm(T)<3.0);
                         }                                                                                             
 			<< make_shared<Hist1D>("He3nCentralGammas6","Events4",Q_axis_full(res))
                         << make_shared<Hist1D>("He3nCentralGammas6","t4",ggggggt)
@@ -305,7 +305,7 @@ void Search3He2Gamma(Analysis&res){
 			<< make_shared<Hist1D>("He3nCentralGammas2","GIM3",ggim)
 			<< make_shared<Hist1D>("He3nCentralGammas2","TIM3",he3ggimdiff)
 		    
-                        <<[ggmm](WTrack&T)->bool{return (ggmm(T)>2.6)&&(ggmm(T)<3.0);}
+                        <<[ggmm](WTrack&T)->bool{return (ggmm(T)>2.5)&&(ggmm(T)<3.0);}
 			<< make_shared<Hist1D>("He3nCentralGammas2","Events4",Q_axis_full(res))
 			<< make_shared<Hist1D>("He3nCentralGammas2","t4",ggt)
 			<< make_shared<Hist1D>("He3nCentralGammas2","dt4",ggdt)
@@ -314,7 +314,7 @@ void Search3He2Gamma(Analysis&res){
 			<< make_shared<Hist1D>("He3nCentralGammas2","GIM4",ggim)
 			<< make_shared<Hist1D>("He3nCentralGammas2","TIM4",he3ggimdiff)
 
-                        <<[ggim](WTrack&T)->bool{return (ggim(T)>0.3)&&(ggim(T)<0.7);}
+                        <<[ggim](WTrack&T)->bool{return (ggim(T)>0.4)&&(ggim(T)<0.7);}
                         << make_shared<Hist1D>("He3nCentralGammas2","Events5",Q_axis_full(res))                                 
                         << make_shared<Hist1D>("He3nCentralGammas2","t5",ggt)
                         << make_shared<Hist1D>("He3nCentralGammas2","dt5",ggdt)
