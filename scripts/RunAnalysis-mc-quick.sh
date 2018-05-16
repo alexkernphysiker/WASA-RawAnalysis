a31ca5345097fedc5422e0ec0bd5e42c445fa29e
rndl-b-2-26 <kernphysiker@openmailbox.org>
2018-05-16 15:32:42 +0200

    additional script

---
+++ scripts/RunAnalysis-mc-quick.sh
@@ -0,0 +1,30 @@
+for X in `seq 1 1 1`; do
+    input="${WMC_DATA}/$1-$X.wmc.data"
+    output="${WASA_OUTPUT_DATA}/MC$1$X"
+    if [ -e ${input} ]; then
+	echo "MC number $X..."
+	if [ -e ${output}.root ];then
+	    echo "...was already done."
+	else
+	    wmc_script="run_wmc-$1-$X.sh"
+	    if [ -e ${wmc_script} ]; then
+		echo "... WMC is stil processing."
+	    else
+	            scriptname="mc$1$X.sh"
+        	    if [ -e ${scriptname} ]; then
+	        	echo "...already in process."
+	    	    else
+			echo "...PROCESSING MC $1..."
+	                echo "#!/bin/bash" >> ${scriptname}
+	                echo "./rawanalysis MC$1 -local -mode mc -fin file:${input} -n ${output} -abort" >> ${scriptname}
+	                echo >> ${scriptname}
+	                echo "rm -f $PWD/${scriptname}" >> ${scriptname}
+	                chmod u+x ${scriptname}
+	                ./${scriptname} &> ${output}.log
+			echo "...done."
+		    fi
+            fi
+        fi
+    fi
+done
+
